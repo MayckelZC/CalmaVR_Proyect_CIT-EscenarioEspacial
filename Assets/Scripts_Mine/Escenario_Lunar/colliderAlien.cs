bbde8bde415fae4f9e2217096b643a30f0ee82e6
Manuel <manuel.arayag22@gmail.com>
2025-10-27 00:29:27 -0300

    TriggerAlien: Panel para inciar actividad

---
+++ Assets/Scripts_Mine/Escenario_Lunar/colliderAlien.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colliderAlien : MonoBehaviour
+{
+
+    public GameObject panelActivar;
+    public string playerTag = "Player";
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            if (panelActivar != null)
+            {
+                panelActivar.SetActive(true);
+            }            
+        }
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            if (panelActivar != null)
+            {
+                panelActivar.SetActive(false);
+            }   
+        }
+    }
+}
